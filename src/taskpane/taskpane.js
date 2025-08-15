a5813099aec37172922b9a808bd2c919c18f4fe6
cactusjack <jackdiges@gmail.com>
2025-08-15 00:27:39 -0400

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   Office.onReady((info) => {
     if (info.host === Office.HostType.Excel) {
       document.getElementById("sideload-msg").style.display = "none";
-      document.getElementById("app-body").style.display = "block";
+      document.getElementById("app-body").style.display = "flex";
       document.getElementById("run").onclick = run;
       document.getElementById("writeCell").onclick = async () => {
         const value = document.getElementById("cellInput").value;
