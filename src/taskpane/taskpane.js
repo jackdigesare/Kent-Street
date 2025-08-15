6b8b3a3af97e3ca7cbfc52ab954cdd25a958bd7c
cactusjack <jackdiges@gmail.com>
2025-08-15 00:29:52 -0400

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 /* global console, document, Excel, Office */
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -10,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     if (info.host === Office.HostType.Excel) {
       document.getElementById("sideload-msg").style.display = "none";
       document.getElementById("app-body").style.display = "flex";
+
       document.getElementById("run").onclick = run;
+
       document.getElementById("writeCell").onclick = async () => {
         const value = document.getElementById("cellInput").value;
         await Excel.run(async (context) => {
